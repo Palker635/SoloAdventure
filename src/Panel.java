3d788463cc73fceb8deeff773099577624c0360f
isac.hansson <Isac.hansson@elev.ga.ntig.se>
2021-01-26 13:25:24 +0100

    Har fixat GUI och funderat på hur jag ska göra med strukturen.

---
+++ src/Panel.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Panel {
+    private JTextArea textArea1;
+    private JTextArea textArea2;
+    private JButton button1;
+    private JButton button2;
+    private JTextArea textArea3;
+}
